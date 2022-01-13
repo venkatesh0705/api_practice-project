7e3c16535876feebde6b8c9c4eb114eb0dc6f83b
venkatesh0705 <venkatesan.m0705@gmail.com>
2022-01-13 18:07:14 +0530

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,68 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <table border="1">
+        <tr>
+          <th>id</th>
+          <th>title</th>
+          <th>body</th>
+        </tr>
+        <tr v-for="data of posts" :key="data.uid">
+          <td>{{ data.id }}</td>
+          <td>{{ data.title }}</td>
+          <td>{{ data.body }}</td>
+        </tr>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
+
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async created() {
+    const Api_url = `https://jsonplaceholder.typicode.com/posts`;
+
+    // async axios with try catch
+    try {
+      let response = await axios.get(Api_url);
+
+      this.posts = await response.data;
+    } catch {
+      (error) => {
+        console.log(error);
+      };
+    }
+
+    //fetch method
+    // fetch(Api_url)
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     this.posts = data;
+    //     console.log(data);
+    //   });
+
+    // axios async without try catch
+    // const response = await axios.get(Api_url);
+
+    // this.posts = response.data;
+
+    // axios.get(Api_url).then((response) => {
+    //   this.posts = response.data;
+    //   console.log(this.posts);
+    // });
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
